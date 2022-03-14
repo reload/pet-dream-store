fc67859f1e3d04b75bfe4f8d7f245760940df8fa
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-03-14 20:03:50 +0100

    Tidying up animal conf in CardGrid component By creating an animal conf object before the rendering we can simplyfy the code

--- app/src/components/CardGrid.tsx
+++ app/src/components/CardGrid.tsx
@@ -26,6 +26,10 @@ const CardGrid: React.FC<Props> = ({ status, isFetching, data }) => {
   return (
     <div className="animal_grid">
       {data.animals.map((animal, index) => {
+        const animalIcon = animalIcons[
+          animal.animal as keyof AnimalIconsInterface
+        ];
+
         return (
           <div className="card" key={index}>
             <img className="card__image" src={animal.img} alt={animal.name} />
@@ -33,10 +37,8 @@ const CardGrid: React.FC<Props> = ({ status, isFetching, data }) => {
             <h2 className="card__headline">{animal.name}</h2>
             <span className="card__type">{animal.type}</span>
             <Animal
-              name={
-                animalIcons[animal.animal as keyof AnimalIconsInterface].name
-              }
-              color={`${animal.animal === "crocodile" ? "#7bb853" : "#f0cc55"}`}
+              name={animalIcon.name}
+              color={animalIcon.color}
             />
             <div className="card__body">
               <p className="card__body__description">{animal.description}</p>
